7787b6534332265ca9b68a0363a4c69a7ec9affd
Luiz Bett <luiz@codx.dev>
2022-08-26 02:13:55 -0300

    no await

--- pages/api/nuvemshop/orderUpdate.jsx
+++ pages/api/nuvemshop/orderUpdate.jsx
@@ -2,44 +2,52 @@ import fetch from "node-fetch";
 import { prisma } from "../../../db";
 
 export default function handler(req, res) {
-//Get data from Coclima about this store access token
-const company = await prisma.companies.findFirst({
+  //Get data from Coclima about this store access token
+  const company = /* await */ prisma.companies.findFirst({
     where: {
       nsid: '"' + req.body.store_id + '"',
     },
-  });  
-  const access_token = company.access_token
-  console.log(company)
+  });
+  const access_token = company.access_token;
+  console.log(company);
 
   //Get data from Order
   const optionsStore = {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json', 'User-Agent': 'CoClima(https://coclima.com)', 'Agent': 'Luiz Bett(luiz@codx.dev', 'Authentication': 'bearer ' + access_token }
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "User-Agent": "CoClima(https://coclima.com)",
+      Agent: "Luiz Bett(luiz@codx.dev",
+      Authentication: "bearer " + access_token,
+    },
   };
-  const urlStore = 'https://api.tiendanube.com/v1/' + req.body.store_id + '/orders/' + req.body.id
-  const requestStore = await fetch(urlStore, optionsStore)
-  const responseStore = await requestStore.json()
-  const date = responseStore.created_at
-  const subtotal = responseStore.subtotal
-  console.log(responseStore)
-
+  const urlStore =
+    "https://api.tiendanube.com/v1/" +
+    req.body.store_id +
+    "/orders/" +
+    req.body.id;
+  const requestStore = /*  await */ fetch(urlStore, optionsStore);
+  const responseStore = /* await */ requestStore.json();
+  const date = responseStore.created_at;
+  const subtotal = responseStore.subtotal;
+  console.log(responseStore);
 
   //Post Receipt with order information
-  const receipt = await prisma.receipts.create({
-        data: {
-          date: date,
-          vendor: "nuvemshop",
-          value: subtotal,
-          order_id: req.body.id,
-          company: {
-            connect: {
-              id: company.id,
-            },
-          },
+  const receipt = /* await  */ prisma.receipts.create({
+    data: {
+      date: date,
+      vendor: "nuvemshop",
+      value: subtotal,
+      order_id: req.body.id,
+      company: {
+        connect: {
+          id: company.id,
         },
-      });
-  
-  console.log(receipt) 
+      },
+    },
+  });
+
+  console.log(receipt);
 
   return res.status(200).json({});
 }
